3fed58df035644d01d6ddb59da0c874996c466db
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-12-19 18:39:09 +0100

    Card creation implemented

--- mrfogg-back/src/main/groovy/org/mrfogg/services/CardService.groovy
+++ mrfogg-back/src/main/groovy/org/mrfogg/services/CardService.groovy
@@ -2,6 +2,8 @@ package org.mrfogg.services
 
 import org.mrfogg.daos.CardDAO
 import org.mrfogg.domains.Card
+import org.mrfogg.domains.User
+import org.mrfogg.domains.Task
 
 class CardService {
 
@@ -13,6 +15,12 @@ class CardService {
 
     Card get(Long id) {
         return this.cardDao.get(id)
+    }
+
+    Card createCardForTraskAndUser(Long taskId, User user, Map params) {
+        Task task = get(taskId)
+
+        return cardDao.create(new Card(title: params.title, owner: user, task: task))
     }
 
 }
